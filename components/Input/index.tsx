d1fc90dae317aca72891a3b0d651637e88d9b75f
Taejin Kim <rlaxowls1316@naver.com>
2024-04-02 09:38:08 +0900

    feat: react-hook-form을 이용한 회원가입, 로그인 폼 구현

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -3,48 +3,65 @@ import eyeOff from "@/assets/eye-off.svg";
 import eyeOn from "@/assets/eye-on.svg";
 import Image from "next/image";
 import { useState } from "react";
-// import { FieldError } from "react-hook-form";
+import {
+  FieldError,
+  UseFormClearErrors,
+  UseFormRegisterReturn,
+} from "react-hook-form";
 
-type Error = {
-  message: string;
+type FormType = {
+  email: string;
+  password: string;
 };
 
 export function Input({
+  id,
   type = "notPassword",
   placeholder,
-  onBlurCallback,
+  register,
   error,
+  clearErrors,
 }: {
+  id: string;
   type?: "password" | "notPassword";
   placeholder?: string;
-  onBlurCallback?: (...args: any[]) => void;
-  error: Error;
+  register: UseFormRegisterReturn;
+  error: FieldError;
+  clearErrors: UseFormClearErrors<FormType>;
 }) {
   const [toggle, setToggle] = useState(false);
+  const onInputChange = register.onChange;
   if (type === "password") {
     return (
-      <div className={styles.field}>
-        <button
-          className={styles.toggleBtn}
-          onClick={(e) => {
-            e.preventDefault();
-            setToggle((prev) => !prev);
-          }}
-        >
-          <Image
-            className={styles.inputIcon}
-            src={!toggle ? eyeOff : eyeOn}
-            alt="eyeIcon"
+      <div>
+        <div className={styles.field}>
+          <span
+            className={styles.toggleBtn}
+            onClick={(e) => {
+              e.preventDefault();
+              setToggle((prev) => !prev);
+            }}
+          >
+            <Image
+              className={styles.inputIcon}
+              src={!toggle ? eyeOff : eyeOn}
+              alt="eyeIcon"
+            />
+          </span>
+          <input
+            {...register}
+            id={id}
+            className={`${styles.input} ${
+              error?.message ? styles.inputError : ""
+            }`}
+            placeholder={placeholder || "비밀번호"}
+            type={!toggle ? "password" : "text"}
+            onChange={(e) => {
+              clearErrors("password");
+              onInputChange(e);
+            }}
           />
-        </button>
-        <input
-          className={`${styles.input} ${
-            error?.message ? styles.inputError : ""
-          }`}
-          placeholder="패스워드"
-          type={!toggle ? "password" : "text"}
-          onBlur={onBlurCallback}
-        />
+        </div>
         {error?.message && (
           <div className={styles.errorMsg}>{error.message}</div>
         )}
@@ -55,12 +72,15 @@ export function Input({
   return (
     <div>
       <input
-        className={`${styles.input} ${
-          error?.message !== "" ? styles.inputError : ""
-        }`}
-        placeholder={placeholder}
+        {...register}
+        id={id}
+        className={`${styles.input} ${error?.message ? styles.inputError : ""}`}
+        placeholder={placeholder || "이메일"}
         type="text"
-        onBlur={onBlurCallback}
+        onChange={(e) => {
+          clearErrors("email");
+          onInputChange(e);
+        }}
       />
       {error?.message && <div className={styles.errorMsg}>{error.message}</div>}
     </div>
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -6,16 +6,32 @@ import HeaderLogo from "@/assets/header-logo.svg";
 import styles from "./styles.module.css";
 import { Input } from "@/components";
 import { FieldError, useForm } from "react-hook-form";
-import { FormEvent } from "react";
-import { postUserSignin } from "@/api/user";
+import { postCheckEmail, postUserSignUp } from "@/api/user";
 import { useRouter } from "next/router";
 
 type FormType = {
   email: string;
   password: string;
+  passwordConfirm: string;
 };
 
-export default function SignIn() {
+type CheckEmailResponseErrorType = {
+  message: string;
+};
+
+async function checkEmail(email: string) {
+  try {
+    await postCheckEmail({ email });
+    return true;
+  } catch (e) {
+    console.log(e);
+    if ((e as CheckEmailResponseErrorType).message === "emailDuplication") {
+      return "이미 사용 중인 이메일입니다.";
+    }
+  }
+}
+
+export default function SignUp() {
   const router = useRouter();
 
   const {
@@ -24,6 +40,7 @@ export default function SignIn() {
     clearErrors,
     handleSubmit,
     setError,
+    watch,
   } = useForm<FormType>({
     mode: "onBlur",
     reValidateMode: "onBlur",
@@ -31,11 +48,10 @@ export default function SignIn() {
 
   const onSubmit = async (user: { email: string; password: string }) => {
     try {
-      const data = await postUserSignin({
+      const data = await postUserSignUp({
         email: user.email,
         password: user.password,
       });
-
       localStorage.setItem("accessToken", data.data.accessToken);
       localStorage.setItem("refreshToken", data.data.refreshToken);
 
@@ -43,15 +59,7 @@ export default function SignIn() {
         router.push("/folder");
       }
     } catch (e) {
-      setError("email", {
-        type: "emailInValid",
-        message: "이메일을 확인해 주세요",
-      });
-      setError("password", {
-        type: "passwordInValid",
-        message: "비밀번호를 확인해 주세요",
-      });
-      // console.log((e as Error).message);
+      console.log(e);
     }
   };
 
@@ -65,48 +73,73 @@ export default function SignIn() {
             </Link>
           </h1>
           <div className={styles["main-header-contents"]}>
-            회원이 아니신가요?
-            <Link href="/signup">회원가입 하기</Link>
+            이미 회원이신가요?
+            <Link href="/signin">로그인 하기</Link>
           </div>
         </div>
         <form id="form" onSubmit={handleSubmit(onSubmit)}>
           <div className={styles.container}>
             <section className={styles.field}>
-              <label htmlFor="email" className={styles.label}>
+              <label className={styles.label} htmlFor="email">
                 이메일
               </label>
               <Input
+                id="email"
                 register={register("email", {
                   required: "이메일을 입력하지 않았습니다.",
                   pattern: {
                     value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
                     message: "올바른 이메일 주소가 아닙니다.",
                   },
+                  validate: checkEmail,
                 })}
-                clearErrors={clearErrors}
-                id="email"
                 error={errors.email as FieldError}
+                clearErrors={clearErrors}
                 placeholder="이메일을 입력해 주세요."
               />
             </section>
             <section className={styles.field}>
-              <label htmlFor="password" className={styles.label}>
+              <label className={styles.label} htmlFor="password">
                 비밀번호
               </label>
               <Input
                 register={register("password", {
                   required: "패스워드를 입력하지 않았습니다.",
+                  pattern: {
+                    value: /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i,
+                    message:
+                      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.",
+                  },
                 })}
                 id="password"
                 type="password"
                 error={errors.password as FieldError}
-                placeholder="비밀번호를 입력해 주세요."
+                placeholder="영문, 숫자를 조합해 8자 이상 입력해 주세요."
+                clearErrors={clearErrors}
+              />
+            </section>
+            <section className={styles.field}>
+              <label className={styles.label} htmlFor="passwordConfirm">
+                비밀번호 확인
+              </label>
+              <Input
+                register={register("passwordConfirm", {
+                  validate: (val: string) => {
+                    if (watch("password") != val) {
+                      return "비밀번호가 일치하지 않아요.";
+                    }
+                  },
+                })}
+                id="passwordConfirm"
+                type="password"
+                error={errors.passwordConfirm as FieldError}
+                placeholder="비밀번호와 일치하는 값을 입력해 주세요."
                 clearErrors={clearErrors}
               />
             </section>
           </div>
           <button type="submit" className="btn type_full">
-            로그인
+            회원가입
           </button>
         </form>
         <div className={styles["main-footer"]}>
